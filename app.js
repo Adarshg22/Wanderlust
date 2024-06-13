8cc8499d79041ca161b76a7773a526888afe182d
Adarsh Gupta <2201031000029@silveroakuni.ac.in>
2024-06-13 15:53:27 +0530

    Done

---
+++ app.js
@@ -0,0 +1,124 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const Listing = require("./models/listing.js");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
+
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+}
+
+main()
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const listingValidate = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  }
+};
+
+//Index Route
+app.get("/listing", async (req, res) => {
+  let allList = await Listing.find({});
+  res.render("./listings/index.ejs", { allList });
+});
+
+//New Route
+app.get("/listing/new", (req, res) => {
+  res.render("./listings/new.ejs");
+});
+
+//Create Route
+app.post(
+  "/listing",
+  listingValidate,
+  wrapAsync(async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listing");
+  })
+);
+
+//Show Route
+app.get(
+  "/listing/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    res.render("./listings/show.ejs", { listing });
+  })
+);
+
+//Edit Route
+app.get(
+  "/listing/:id/edit",
+  listingValidate,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    res.render("./listings/edit.ejs", { listing });
+  })
+);
+
+//Update Route
+app.put(
+  "/listing/:id",
+  wrapAsync(async (req, res) => {
+    if (!req.body.listing) {
+      throw new ExpressError(400, "Send valid Data for listing");
+    }
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listing/${id}`);
+  })
+);
+
+//Destroy Route
+app.delete(
+  "/listing/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listing");
+  })
+);
+
+//Universal Route
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found!"));
+});
+
+//Error Handling Middleware
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went Wrong" } = err;
+  res.status(statusCode).render("./listings/error.ejs", { message });
+});
+
+app.get("/", (req, res) => {
+  res.send("Server word properly");
+});
+
+app.listen(3000, () => {
+  console.log("Port is listening");
+});
---
+++ models/listing.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const listingSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    filename: String,
+    url: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1542718610-a1d656d1884c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y296eSUyMGNhYmlufGVufDB8fDB8fHww",
+    },
+  },
+  price: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
