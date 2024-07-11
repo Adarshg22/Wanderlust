f1ac626db503fda2d4439d3c051c3066b648f7bc
Adarsh Gupta <2201031000029@silveroakuni.ac.in>
2024-07-11 11:58:02 +0530

    Add User Authentication function in the project

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listing = require("./routes/listing.js");
-const review = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
@@ -50,17 +54,37 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 });
 
+// app.get("/demo", async (req, res) => {
+//   let fakeuser = new User({
+//     email: "adarshgupta.1312@gmail.com",
+//     username: "Adarsh Gupta",
+//   });
+
+//   let result = await User.register(fakeuser, "adarsh92000");
+//   res.send(result);
+// });
+
 //Listing
-app.use("/listing", listing);
+app.use("/listing", listingRouter);
 
 //Review
-app.use("/listing/:id/reviews", review);
+app.use("/listing/:id/reviews", reviewRouter);
+
+//SignUp
+app.use("/", userRouter);
 
 //Universal Route
 app.all("*", (req, res, next) => {
---
+++ routes/user.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
+
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
+});
+
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+      await User.register(newUser, password);
+      req.flash("success", "Welcome to Wanderlust!");
+      res.redirect("/listing");
+    } catch (err) {
+      req.flash("error", err.message);
+      res.redirect("/signup");
+    }
+  })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome back to wanderlust!");
+    res.redirect("/listing");
+  }
+);
+
+module.exports = router;
